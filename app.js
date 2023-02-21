dc6cd39151011f4ed83326bb10fbcd03e15aef68
Hasan Badshah <mharis.javed1996@gmail.com>
2023-02-21 20:24:54 +0500

    install express js in the package

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 const http = require("http");
-const routes =  require('./routes');
-const server = http.createServer(routes);
+const express = require("express");
+const app = express();
+app.use((req,res,next)=>{
+    console.log("1st middleware is working");
+    next(); // Allows the request to continue to the next middleware in the line
+});
+
+app.use((req,res,next)=>{
+    console.log("2nd middleware is working");
+    res.send("<h1>This is working absolutely fine</h1>");
+    next();
+});
+// const routes =  require('./routes');
+// const server = http.createServer(routes);
+const server = http.createServer(app);
 server.listen(3000);
