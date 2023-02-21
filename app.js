b2f12a073d865b743c5d88cd9f260a6630ae1b6c
Hasan Badshah <mharis.javed1996@gmail.com>
2023-02-22 00:20:14 +0500

    using post insted of use

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use("/add-product",(req,res,next)=>{
     res.send("<form method='POST' action='/product'><input type='text' name='title'/><input type='submit' value='submit'/></form>");
 });
 
-app.use("/product",(req,res,next)=>{
+app.post("/product",(req,res,next)=>{
     console.log(req.body);
     res.redirect("/");
 });
