41cf8c080a5e4c8cf8004c48cd024259231b98a1
Muhammad Haris Javed <mharis.javed1996@gmail.com>
2023-03-02 00:58:22 +0500

    make a file and added data in the file

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,12 @@
 
 const Product = require("../models/product");
 exports.getAddPorduct=(req,res,next)=>{
-    res.render('add-product',{
-        pageTitle:"Add Product",
-        path:"/admin/add-product"
+    res.render('add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
     });
 };
 
@@ -14,10 +17,14 @@ exports.postAddProduct=(req,res,next)=>{
 };
 
 exports.getAddProduct=(req,res,next)=>{
-    const products = Product.fetchAll() ;
-    res.render('shop',{
-        prods:products,
-        pageTitle:"Shop",
-        path:"/"
+    Product.fetchAll(products => {
+        res.render('shop', {
+          prods: products,
+          pageTitle: 'Shop',
+          path: '/',
+          hasProducts: products.length > 0,
+          activeShop: true,
+          productCSS: true
+        });
     });
 };
--- models/product.js
+++ models/product.js
@@ -1,16 +1,39 @@
-const products = [];
-module.exports= class Product {
-    constructor(t){
-        this.title = t;
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(require.main.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
+  });
+};
 
-    save(){
-        products.push(this);
-    }
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
 
-    static fetchAll(){
-        return products;
-    }
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        if(err){
+            console.log(err);
+        }
+      });
+    });
+  }
 
-
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
 };
