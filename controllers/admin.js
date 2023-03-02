3746cc3c01d436086bfc509da19c2d2e8a4b350c
Muhammad Haris Javed <mharis.javed1996@gmail.com>
2023-03-02 10:24:21 +0500

    Registered the routes

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,9 +4,6 @@ exports.getAddPorduct=(req,res,next)=>{
     res.render('admin/add-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
-        formsCSS: true,
-        productCSS: true,
-        activeAddProduct: true
     });
 };
 
@@ -16,15 +13,12 @@ exports.postAddProduct=(req,res,next)=>{
     res.redirect("/");
 };
 
-exports.getAddProduct=(req,res,next)=>{
+exports.getProducts=(req,res,next)=>{
     Product.fetchAll(products => {
-        res.render('shop/product-list', {
+        res.render('admin/products', {
           prods: products,
-          pageTitle: 'Shop',
-          path: '/',
-          hasProducts: products.length > 0,
-          activeShop: true,
-          productCSS: true
+          pageTitle: 'Admin Products',
+          path: '/admin/products'
         });
     });
 };
