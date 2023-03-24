9e8caeaa586e183476d04b29cb0502d19cf67858
muhammad-afzal-melon <mharis.javed1996@gmail.com>
2023-03-25 01:16:03 +0500

    created a dummy user

--- app.js
+++ app.js
@@ -30,6 +30,15 @@ const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
 
+
+app.use((req, res, next) => {
+    User.findById(1)
+      .then(user => {
+        req.user = user;
+        next();
+      })
+      .catch(err => console.log(err));
+  });
 // routes which start with /admin will execute line 14  and then will not conside /admin
 app.use("/admin",adminRoutes);
 app.use(shopRoutes);
@@ -37,13 +46,21 @@ app.use(shopRoutes);
 // handling 404 page
 app.use(errorController.get404);
 
+// line 41 means if a user delete and all the products related to user will also delete
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
-sequelize
+
 // .sync({ force: true }).then(result => {
-.sync().then(result => {
+sequelize.sync().then(result => {
     // console.log(result);
-    app.listen(3001);}
-    ) .catch(err => {
+    return User.findByPk(1);
+    }).then((user)=>{
+        if(!user){
+            User.create({name:'haris12',email:'haris@gmail.com'});
+        }
+        return user;
+    }).then(user=>{
+        app.listen(3001);
+    }).catch(err => {
     console.log("not created",err);
 });
