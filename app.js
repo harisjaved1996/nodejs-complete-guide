33e08d646e156862ecf78ba95f25e3ce394d1cdd
Muhammad Haris Javed <mharis.javed1996@gmail.com>
2023-04-06 21:17:35 +0500

    created product schema 212

--- app.js
+++ app.js
@@ -24,21 +24,21 @@ const errorController = require("./controllers/error");
 const mongoose = require('mongoose');
 const User = require("./models/user");
 
-app.use((req, res, next) => {
-  User.findById('6425fb41164e9a7868aad29f').then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      console.log("req user",req.user);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('6425fb41164e9a7868aad29f').then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       console.log("req user",req.user);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 // routes which start with /admin will execute line 14  and then will not conside /admin
 app.use("/admin",adminRoutes);
 app.use(shopRoutes);
 
 // handling 404 page
 app.use(errorController.get404);
-mongoose.connect('mongodb://127.0.0.1:27017/ecom').then(result=>{
+mongoose.connect('mongodb://127.0.0.1:27017/shop').then(result=>{
   console.log("app connected with database");
   app.listen(3000);
 }).catch(error=>{
