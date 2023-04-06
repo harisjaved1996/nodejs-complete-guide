dee75eb6290a49f752f6c97964be5cb742850493
Muhammad Haris Javed <mharis.javed1996@gmail.com>
2023-04-06 22:28:22 +0500

    deleting product through mongoose 217

--- controllers/admin.js
+++ controllers/admin.js
@@ -85,7 +85,7 @@ exports.getProducts=(req,res,next)=>{
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId).then(()=>{
+  Product.findByIdAndRemove(prodId).then(()=>{
     console.log("product Deleted in controller");
     res.redirect('/admin/products');
   }).catch((error)=>{
