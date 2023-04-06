1b5ba4c4466f3f7c1e4a465117cb62c58d054421
Muhammad Haris Javed <mharis.javed1996@gmail.com>
2023-04-06 23:19:17 +0500

    adding a user and using user model through mongoose 218

--- app.js
+++ app.js
@@ -24,14 +24,14 @@ const errorController = require("./controllers/error");
 const mongoose = require('mongoose');
 const User = require("./models/user");
 
-// app.use((req, res, next) => {
-//   User.findById('6425fb41164e9a7868aad29f').then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       console.log("req user",req.user);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('642f0a5a8723761433b4ede2').then(user => {
+      req.user = user;
+      console.log("req user",req.user);
+      next();
+    })
+    .catch(err => console.log(err));
+});
 // routes which start with /admin will execute line 14  and then will not conside /admin
 app.use("/admin",adminRoutes);
 app.use(shopRoutes);
@@ -40,6 +40,19 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 mongoose.connect('mongodb://127.0.0.1:27017/shop').then(result=>{
   console.log("app connected with database");
+  User.findOne().then(user=>{
+    console.log(user);
+    if(!user){
+      const user = new User({
+        name:'haris',
+        email:'haris@yahoo.com',
+        cart: {
+          items:[]
+        }
+      });
+      user.save();
+    }
+  });
   app.listen(3000);
 }).catch(error=>{
   console.log("app did not connect with the mongodb",error);
