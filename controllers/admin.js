cc0bb648ca3ef040bfc7bcea7e065d941c7e0da2
Muhammad Haris Javed <mharis.javed1996@gmail.com>
2023-04-07 00:11:41 +0500

    added relations in mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -38,7 +38,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product({title, price, description, imageUrl});
+  const product = new Product({
+    title:title, 
+    price:price,
+    description:description,
+    imageUrl:imageUrl,
+    userId:req.user
+  });
   product.save().then(result => {
       // console.log(result);
       console.log('Created Product');
