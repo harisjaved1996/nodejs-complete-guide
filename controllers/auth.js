939ac658b11b8e2b6ce3aff76f6bc02afd021476
Muhammad Haris <mharis.javed1996@gmail.com>
2023-04-10 16:02:45 +0500

    completed signin functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,15 +16,32 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('642f0a5a8723761433b4ede2')
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
     .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      console.log("req.session.user",req.session.user);
-      res.redirect('/');
+      if (!user) {
+        return res.redirect('/login');
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save(err => {
+              console.log(err);
+              res.redirect('/');
+            });
+          }
+          res.redirect('/login');
+        })
+        .catch(err => {
+          console.log(err);
+          res.redirect('/login');
+        });
     })
     .catch(err => console.log(err));
-  
 };
 
 exports.getSignup = (req, res, next) => {
