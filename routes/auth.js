84430ee97a154d6b74631f00680c64a9c9a9d41a
Muhammad Afzal <mharis.javed1996@gmail.com>
2023-04-16 09:49:05 +0500

    added custom message for invalid email using express-validator package

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
 
 router.get('/signup', authController.getSignup);
-router.post('/signup', check('email').isEmail(), authController.postSignup);
+router.post('/signup', check('email').isEmail().withMessage('Please enter a valid email.'), authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
