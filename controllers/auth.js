efd28b6395a963435455427a536d99e1bf5a5cfb
Muhammad Afzal <mharis.javed1996@gmail.com>
2023-04-16 09:40:39 +0500

    added basic validation on postSignUp endpoint

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 // for mail
 const nodemailer = require('nodemailer');
+const { validationResult } = require('express-validator');
 // const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 // const transporter = nodemailer.createTransport(
@@ -88,6 +89,15 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()
+    });
+  }
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
