9c9aaee8d9b8502e66f44c48a148b0d640c22b77
Muhammad Afzal <mharis.javed1996@gmail.com>
2023-04-22 00:36:08 +0500

    storing file data in the database and serving images statically

--- controllers/admin.js
+++ controllers/admin.js
@@ -46,10 +46,24 @@ exports.getEditProduct = (req, res, next) => {
 exports.postAddProduct = (req, res, next) => {
   // const prodId = req.body.productId;
   const title = req.body.title;
-  const imageUrl = req.file;
+  const image = req.file;
   const price = req.body.price;
   const description = req.body.description;
-  console.log("=====imageUrl=====",imageUrl);
+  if (!image) {
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        price: price,
+        description: description
+      },
+      errorMessage: 'Attached file is not an image.',
+      validationErrors: []
+    });
+  }
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -69,7 +83,7 @@ exports.postAddProduct = (req, res, next) => {
       validationErrors: errors.array()
     }); 
   }
-
+  const imageUrl = image.path;
   const product = new Product({
     title: title,
     price: price,
@@ -94,7 +108,7 @@ exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
   const updatedTitle = req.body.title;
   const updatedPrice = req.body.price;
-  const updatedImageUrl = req.body.imageUrl;
+  const image = req.file;
   const updatedDesc = req.body.description;
 
   const errors = validationResult(req);
@@ -124,8 +138,10 @@ exports.postEditProduct = (req, res, next) => {
       }
       product.title = updatedTitle;
       product.price = updatedPrice;
+      if (image) {
+        product.imageUrl = image.path;
+      }
       product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
       return product.save().then(result => {
         console.log('UPDATED PRODUCT!');
         res.redirect('/admin/products');
